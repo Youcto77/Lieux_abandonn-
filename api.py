82e5cff817587110d10c8538a27724dc2c0dca4f
Youcto_DEV <dropeddroped2@gmail.com>
2025-11-08 00:50:46 +0100

    First

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/images/search', methods=['GET'])
+def get_dog_image():
+    data = [
+        {
+            "id": "Z6h74tJ-D",
+            "url": "https://cdn2.thedogapi.com/images/Z6h74tJ-D.jpg",
+            "width": 1080,
+            "height": 1080
+        }
+    ]
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
